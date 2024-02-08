27d467c7e419bd6e56f6a60a6692237347f2241d
m.essa <ostora27@gmail.com>
2024-02-09 00:38:14 +0200

    v4

--- js/myJquery.js
+++ js/myJquery.js
@@ -1,57 +1,39 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
-
 $(document).ready(function () {
   'use strict';
-
-
-
-
   $(function () {
     $('#datetimepicker54').datetimepicker({
       daysOfWeekDisabled: [0, 2, 3, 4, 5, 6],
       format: 'L'
     });
   });
-
-
-
   $(document).on('scroll', function () {
     var headr = $("#mainNav");
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       headr.addClass('scrolled');
     } else {
       headr.removeClass('scrolled');
-
     }
   });
-
-
   $(function () {
     var headr = $("#mainNav");
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       headr.addClass('scrolled');
     } else {
       headr.removeClass('scrolled');
-
     }
-
   });
-
   $(".side-open").click(function () {
     $('#fixed-side').addClass("active");
     $("body").addClass("modal-open");
     $('#lay').addClass("active");
-
   });
   $(".side-close").click(function () {
     $('#fixed-side').removeClass("active");
     $("body").removeClass("modal-open");
     $('#lay').removeClass("active");
   });
-
-
-
   var swiper = new Swiper('.pic-slider', {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -61,25 +43,16 @@ $(document).ready(function () {
       nextEl: '.pic-next',
       prevEl: '.pic-prev',
     },
-
-
   });
-
   var swiper2 = new Swiper('.vid-slider', {
-
     observer: true,
     observeParents: true,
     loop: true,
     navigation: {
       nextEl: '.bb22',
       prevEl: '.bb33',
-
     },
   });
-
-
-
-
   var swiper3 = new Swiper('.pic-slider2', {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -89,27 +62,14 @@ $(document).ready(function () {
       nextEl: '.pic-next2',
       prevEl: '.pic-prev2',
     },
-
-
   });
-
   var swiper4 = new Swiper('.vid-slider2', {
-
     observer: true,
     observeParents: true,
     loop: true,
     navigation: {
       nextEl: '.bb222',
       prevEl: '.bb332',
-
     },
   });
-
-
-
-
-
-
-
-
 });
